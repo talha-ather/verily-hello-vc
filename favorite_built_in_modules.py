1cb3d7a73550d07c089ed4073672099445c6ce75
Vihang Mangalvedhekar <vihangm96@gmail.com>
2022-02-18 16:54:01 -0700

    Update favorite_built_in_modules.py (#22)

--- favorite_built_in_modules.py
+++ favorite_built_in_modules.py
@@ -24,7 +24,6 @@ import os
 import array
 import sqlite3
 
-
 def main():
     ###########################################################################
     #
@@ -33,9 +32,8 @@ def main():
     ###########################################################################
     print("John DiMatteo likes datetime:", datetime.__doc__)
     print("Foo likes math:", math.__doc__)
-    print("Vihang likes os")
+    print("Vihang likes os and array")
     print("Foo also likes array and sqlite3")
-
 
 if __name__ == "__main__":
     main()
